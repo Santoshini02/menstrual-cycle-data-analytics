913479ac82667fd0472839017120cf867b16c3f3
Santoshini02 <124santoshini2005@sjcem.edu.in>
2026-03-13 21:17:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const feather = document.querySelector(".feather");
+
+    if (feather) {
+        feather.style.animation = "fall 5s linear infinite";
+    }
+
+});
